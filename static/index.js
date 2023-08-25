9389b01f65033bb2a63f093ef75572a5783f0cbf
dpapanicker <70652939+dpapanicker@users.noreply.github.com>
2023-08-25 17:21:21 +0530

    Add files via upload

---
+++ static/index.js
@@ -0,0 +1,61 @@
+$(document).ready(function () {
+    getStory();
+})
+
+$(function () {
+    $(".input_field").keyup(function () {
+        let id = $(this).attr("id");
+        let input_number = id.split("_")[1]
+        $(".rep_input").eq(input_number).html($(this).val());
+    })
+
+    $("#submit_story").click(function () {
+        
+
+
+
+        $.ajax({
+           
+
+
+
+
+        })
+    })
+})
+
+function getStory() {
+    $.ajax({
+        
+
+
+
+
+    })
+}
+
+function displayStory(story) {
+    $("#story_title").html(story.title)
+
+    $("#bank_words").empty();
+    for (let i = 0; i < story.words.length; i++) {
+        let html = `<button class="word_bank_button">${story.words[i]}</button>`
+        $("#bank_words").append(html)
+    }
+
+    $("#input_fields").empty();
+    for (let i = 0; i < story.inputs; i++) {
+        let input_html = `<input type="text" class="input_field" id="input_${i}" placeholder="Input ${i + 1}"/>`
+        $("#input_fields").append(input_html)
+    }
+
+    $("#story_text").html(story.story)
+    $("#story_id").val(story.story_id)
+
+    // Move  the input keyup listener here
+
+
+
+
+    
+}
